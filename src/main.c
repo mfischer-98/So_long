c172e48b44925be3fdc0055b0d7f26be9fd0beaa
mfischer-98 <mefische@c1r13s8.42porto.com>
2025-06-20 15:43:17 +0100

    started project

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include "so_long.h"
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
+
+int	main(void)
+{
+	void	*mlx;
+	t_data	img;
+
+	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	mlx_loop(mlx);
+}
---
+++ src/so_long.h
@@ -0,0 +1,6 @@
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+#include "../lib/mlx/mlx.h" 
+
+#endif
